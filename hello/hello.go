6d7e70d5c1d728274e7569841e892e0933f8db2c
ElPapi42 <whitman-2@hotmail.com>
2020-03-27 13:09:41 -0400

    Added strutil package

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/elpapi42/go-start/strutil"
+)
 
 func main() {
-	fmt.Printf("hello, world!\n")
+	fmt.Printf(strutil.Reverse("hello"))
 }
