eed753f6d176c36ec0375c3f77bbcb2ce2d3046c
ElPapi42 <whitman-2@hotmail.com>
2020-03-29 10:18:41 -0400

    Added practice package

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/google/go-cmp/cmp"
-
 	"github.com/elpapi42/go-start/phone"
+	"github.com/elpapi42/go-start/practice"
 	"github.com/elpapi42/go-start/strutil"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
@@ -15,4 +16,6 @@ func main() {
 
 	mia3 := phone.New("Mi A3", "Xiaomi", 6)
 	mia3.Features()
+
+	practice.CreateVars()
 }
---
+++ practice/vars.go
@@ -0,0 +1,15 @@
+package practice
+
+import (
+	"fmt"
+)
+
+// CreateVars and print them
+func CreateVars() {
+	var (
+		name = "whitman"
+		age  int
+	)
+
+	fmt.Println(name, age, "Years")
+}
